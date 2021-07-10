9138b79dba14622e314485c5c07d9f48ac5f17c0
Lucca <86019732+ndlucca@users.noreply.github.com>
2021-07-10 00:51:17 +0000

    atualização final

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-function validaFormulario(){
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
-  if(document.getElementById("nome").value != "" &&
-     document.getElementById("email").value != "" &&
-     document.getElementById("telefone").value != "" ){
-
-    alert("Prontinho! você receberá as novidades por email.")
-  }else{
-    alert("Por favor, preencha os campos nome e email.")
+function validaFormulario()
+{ 
+  if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value !="")
+  {
+    alert("Prontinho! Você receberá as novidades por email.")
   }
-}
+  else{
+    alert("Por favor, preencha os campos nome, email e telefone.")
+  }
+} 
